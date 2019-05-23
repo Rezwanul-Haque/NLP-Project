50555c740a66e229dc723b448456c6534fd17707
Rezwanul-Haque <rezwanul.cse@gmail.com>
2019-05-23 16:29:36 +0600

    Converting text to features inital commit

---
+++ Converting_Text_to_Features/recipe_1_one_hot_encoding/main.py
@@ -0,0 +1,23 @@
+""" Project_name
+    Problem
+
+    
+    Definition
+        
+    Solution
+
+    
+    Author: Rezwanul Haque
+    Date: 
+    License: MIT
+"""
+# Import Standard Library
+
+
+# Import Third Party Library
+
+
+# Import Local  Library
+
+
+# Your code goes here
